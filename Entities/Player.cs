a5febebfd9de8b017104d72acb53405d84565399
Nayu2714 <kaminayu2714@gmail.com>
2026-07-05 15:02:37 +0900

    Playerアイテム取得用インベントリListの追加

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -1,11 +1,21 @@
+using simple_dungeon_rpg.Items;
+
 namespace simple_dungeon_rpg.Entities;
 
 public class Player : Entity
 {
     public override char Symbol => '@';
+    
+    public IReadOnlyList<Item> Inventory => _inventory;
+    private readonly List<Item> _inventory;
 
     public Player(int y, int x) : base(y, x, maxHp: 20, hp: 20, atk: 2)
     {
-        
+        _inventory = new List<Item>();
+    }
+
+    public void AddItem(Item item)
+    {
+        _inventory.Add(item);
     }
 }
